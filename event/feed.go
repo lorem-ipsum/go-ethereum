b942aa79fcd54c4c03f27bcd926d46780eb10d3a
clt <1330525233@qq.com>
2021-06-03 16:09:25 +0800

    event/feed.go

--- event/feed.go
+++ event/feed.go
@@ -33,6 +33,7 @@ var errBadChannel = errors.New("event: Subscribe argument does not have sendable
 //
 // The zero value is ready to use.
 type Feed struct {
+	// @notes sendCases是订阅者列表，inbox是刚刚加入的订阅者列表，Send函数会将inbox的订阅者移动到sendCases中。
 	once      sync.Once        // ensures that init only runs once
 	sendLock  chan struct{}    // sendLock has a one-element buffer and is empty when held.It protects sendCases.
 	removeSub chan interface{} // interrupts Send
@@ -61,6 +62,7 @@ func (f *Feed) init() {
 	f.removeSub = make(chan interface{})
 	f.sendLock = make(chan struct{}, 1)
 	f.sendLock <- struct{}{}
+	// @notes sendCases[0]为removeSub
 	f.sendCases = caseList{{Chan: reflect.ValueOf(f.removeSub), Dir: reflect.SelectRecv}}
 }
 
@@ -86,6 +88,7 @@ func (f *Feed) Subscribe(channel interface{}) Subscription {
 	}
 	// Add the select case to the inbox.
 	// The next Send will add it to f.sendCases.
+	// @notes 通道ch构造一个SelectCase对象，然后将其加入到inbox数组中。这样就完成了通道的订阅。
 	cas := reflect.SelectCase{Dir: reflect.SelectSend, Chan: chanval}
 	f.inbox = append(f.inbox, cas)
 	return sub
@@ -103,8 +106,10 @@ func (f *Feed) typecheck(typ reflect.Type) bool {
 func (f *Feed) remove(sub *feedSub) {
 	// Delete from inbox first, which covers channels
 	// that have not been added to f.sendCases yet.
+	// @notes 用ch来表征待删除的sub
 	ch := sub.channel.Interface()
 	f.mu.Lock()
+	// @notes 先尝试从f.inbox这个slice中删除ch
 	index := f.inbox.find(ch)
 	if index != -1 {
 		f.inbox = f.inbox.delete(index)
@@ -113,12 +118,16 @@ func (f *Feed) remove(sub *feedSub) {
 	}
 	f.mu.Unlock()
 
+	// @notes 若f.inbox中不存在ch，则尝试从sendCases中删除ch（因为ch不可能同时存在于二者之中）
 	select {
 	case f.removeSub <- ch:
+		// 在Send函数中会处理此情况
 		// Send will remove the channel from f.sendCases.
 	case <-f.sendLock:
+		// @notes 因为当Send函数运行时，会先receive: <-f.sendLock。因此若为本case，则Send未在运行。
 		// No Send is in progress, delete the channel now that we have the send lock.
 		f.sendCases = f.sendCases.delete(f.sendCases.find(ch))
+		// @notes 开锁，为下次Send作准备
 		f.sendLock <- struct{}{}
 	}
 }
@@ -129,14 +138,17 @@ func (f *Feed) Send(value interface{}) (nsent int) {
 	rvalue := reflect.ValueOf(value)
 
 	f.once.Do(f.init)
+	// @notes 读sendLock通道，当sendLock为空时堵塞（可以看出如果要上锁，则<-sendLock）。
 	<-f.sendLock
 
 	// Add new cases from the inbox after taking the send lock.
 	f.mu.Lock()
+	// @notes 将f.inbox注入到f.sendCases中
 	f.sendCases = append(f.sendCases, f.inbox...)
 	f.inbox = nil
 
 	if !f.typecheck(rvalue.Type()) {
+		// @notes 出错了，退出前先写sendLock以免下次send操作堵塞
 		f.sendLock <- struct{}{}
 		f.mu.Unlock()
 		panic(feedTypeError{op: "Send", got: rvalue.Type(), want: f.etype})
@@ -144,6 +156,7 @@ func (f *Feed) Send(value interface{}) (nsent int) {
 	f.mu.Unlock()
 
 	// Set the sent value on all channels.
+	// @notes 先为所有通道设置将要发送的数据
 	for i := firstSubSendCase; i < len(f.sendCases); i++ {
 		f.sendCases[i].Send = rvalue
 	}
@@ -156,19 +169,23 @@ func (f *Feed) Send(value interface{}) (nsent int) {
 		// Fast path: try sending without blocking before adding to the select set.
 		// This should usually succeed if subscribers are fast enough and have free
 		// buffer space.
+		// @notes 在每次循环中，先尝试通过非阻塞的方式发送
 		for i := firstSubSendCase; i < len(cases); i++ {
 			if cases[i].Chan.TrySend(rvalue) {
 				nsent++
+				// @notes 相当于从cases这个slice中删除第i项
 				cases = cases.deactivate(i)
 				i--
 			}
 		}
+		// @notes 倘若所有通道都已经发送完成
 		if len(cases) == firstSubSendCase {
 			break
 		}
 		// Select on all the receivers, waiting for them to unblock.
 		chosen, recv, _ := reflect.Select(cases)
 		if chosen == 0 /* <-f.removeSub */ {
+			// @notes 由remove函数中f.removeSub <- ch触发，故分别于f.sendCases和cases中删除接收到的ch
 			index := f.sendCases.find(recv.Interface())
 			f.sendCases = f.sendCases.delete(index)
 			if index >= 0 && index < len(cases) {
@@ -182,9 +199,11 @@ func (f *Feed) Send(value interface{}) (nsent int) {
 	}
 
 	// Forget about the sent value and hand off the send lock.
+	// @notes 重置待发送的信息
 	for i := firstSubSendCase; i < len(f.sendCases); i++ {
 		f.sendCases[i].Send = reflect.Value{}
 	}
+	// @notes 返回前写入f.sendLock，为下一次发送作准备
 	f.sendLock <- struct{}{}
 	return nsent
 }
